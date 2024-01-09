bad07af41945a306e99a3fed3c7baff94bc61307
kwaw kumi <kwawkumi9@gmail.com>
2024-01-09 12:11:03 +0000

    changenotifier

---
+++ lib/commons/ThemeManager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:vodafone/main.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _currentTheme = lightTheme;
+
+  ThemeData get currentTheme => _currentTheme;
+
+  void toggleTheme() {
+    _currentTheme =
+        (_currentTheme == lightTheme) ? darkTheme : lightTheme;
+    notifyListeners();
+  }
+}
