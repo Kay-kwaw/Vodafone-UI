93b1e62c666b41bdafcd9aa4e3846b7628deaa81
kwaw kumi <kwawkumi9@gmail.com>
2024-01-09 11:30:29 +0000

    themedata

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(), // Light theme as a fallback
-      darkTheme: ThemeData.dark().copyWith(
-        // Customize your dark theme colors here
-        primaryColor: Colors.red,
-        hintColor: Colors.white,
-        // ... other theme properties
-      ),
-      themeMode: ThemeMode.system, // or ThemeMode.light or ThemeMode.dark
+    return  MaterialApp(
+     theme: ThemeData.light(),
+     themeMode: ThemeMode.light,
+     darkTheme: ThemeData.dark(),
       home: const Homepage(),
     );
   }
